989539bba01402ecf0746b462fe194d05bce8c77
Craig Cook <boycook@me.com>
2013-08-06 18:25:35 +0100

    Checking dir repo matches before doing pull

--- lib/pull.sh
+++ lib/pull.sh
@@ -1,6 +1,16 @@
 #!/bin/bash 
 
 REPO_DIR=$1
+SSH_URL=$2
+HTTPS_URL=$3
 
 cd $REPO_DIR
-git pull
+
+REMOTE_URL=`git config --get remote.origin.url`
+
+if [[ "$REMOTE_URL" == *$SSH_URL* ]] || [[ "$REMOTE_URL" == *$HTTPS_URL* ]]
+then
+	git pull
+else 
+	echo "Ignoring non users repository [$REMOTE_URL] @ [$REPO_DIR]";
+fi
