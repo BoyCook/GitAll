27a4c1dfb2181998f08fad4ceaa18476a6eb7bdc
Craig Cook <craig.cook@catapult.cx>
2026-02-18 12:45:25 +0000

    Remove legacy Node.js source files

--- lib/clone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-ROOT_DIR=$1
-REPO=$2
-TARGET=$3
-
-cd $ROOT_DIR
-git clone $REPO $TARGET
--- test/spec/GitAllSpec.js
+++
@@ -1,8 +0,0 @@
-var should = require('should');
-var GitAll = require('../../lib/gitall');
-
-describe('GitAll', function () {
-    it('should validate ok', function () {
-        new GitAll('user', './', 'ssh');
-    });
-});
